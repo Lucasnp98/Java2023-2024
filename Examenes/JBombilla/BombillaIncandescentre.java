95f1908ae7c73bb1c3f3f3e4e8167963202d8a7f
Lucasnp98 <108325074+Lucasnp98@users.noreply.github.com>
2024-03-20 09:26:07 +0100

    Delete Examenes/JBombilla directory

--- Examenes/JBombilla/BombillaIncandescentre.java
+++
@@ -1,9 +0,0 @@
-public class BombillaIncandescente extends Bombilla{
-
-  public BombillaIncandescente(int luminosidad, int vatios){
-    super(luminosidad, vatios);
-  }  
-  
-
-
-}
--- Examenes/JBombilla/Eficienciable.java
+++
@@ -1,3 +0,0 @@
-public interface Eficienciable{
-  public double getEficienciaEnergetica();  
-}
